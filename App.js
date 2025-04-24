2e47a39de50c6ebe9d9b568406eefe6821fa42d3
Giuseppe Mancini <giuseppe.mancini80@gmail.com>
2025-04-24 12:15:31 +0200

    firebase configutation

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
 import { StatusBar } from "expo-status-bar";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 import { StyleSheet, Text, View } from "react-native";
 import Start from "./components/Start";
 import Chat from "./components/Chat";
@@ -11,6 +13,14 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+  const firebaseConfig = {
+    apiKey: "AIzaSyBLGnmOHGUtnFSHyIOb5yQwItNJkiMRWd0",
+    authDomain: "shopping-list-demo-5ef3a.firebaseapp.com",
+    projectId: "shopping-list-demo-5ef3a",
+    storageBucket: "shopping-list-demo-5ef3a.firebasestorage.app",
+    messagingSenderId: "338378377371",
+    appId: "1:338378377371:web:093927c42c713ca9201e3e",
+  };
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Start">
@@ -20,6 +30,12 @@ const App = () => {
     </NavigationContainer>
   );
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
 
 const styles = StyleSheet.create({});
 
